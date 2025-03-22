789dda9da123d91501ab4b9ce3e028dca42eaf22
Kenneth Stanley <kstanley_bass@hotmail.com>
2025-03-22 20:41:06 -0300

    [Aplicando for]

---
+++ loop/desafio01.py
@@ -0,0 +1,10 @@
+"""
+    Usando um loop, exiba na tela: Estamos em X onde x é um valor iniciado em 18 e finalizando  em 110.
+"""
+
+for numero in range(18, 111):
+    print('Estamos em', numero) 
+
+
+
+
---
+++ loop/desafio2.py
@@ -0,0 +1,9 @@
+"""
+    Você precisa de 10 passos para finalizar uma tarefa, exiba na tela, usando llop for  a segunte fraze "Realizando Passo X"
+"""
+
+tarefa = ['passo 01', 'passo 02', 'passo 03', 'passo 04', 'passo 05',
+          'passo 06', 'passo 07', 'passo 08', 'passo 09', 'passo 10']
+
+for item in tarefa:
+    print('Realizando ', item)
